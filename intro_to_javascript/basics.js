b29773a70a3e10ab189071a5971f8e08e3ac268e
Rachel Lin <rachel.lynch.lin@gmail.com>
2019-02-07 22:00:23 -0800

    The basics of variables with JavaScript

---
+++ intro_to_javascript/basics.js
@@ -0,0 +1,20 @@
+// Variables
+// Variables are simply containers that store values
+// They follow this patternL
+var yourVariable = yourValue;
+
+// They can store of the data types we have seen
+var name = "Rusty";
+var secretNumber = 73;
+var isAdorable = true;
+
+// Recall the stored value by calling the variable name
+var name = "Rusty";
+"hello there " + name   // "hello there Rusty"
+
+var num = 36;
+num + 3 + 10    // 49
+
+// We can also update existing variables
+var name = "Robert";
+name = "Bob";
