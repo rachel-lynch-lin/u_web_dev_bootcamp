4a3ab0663717188a6c8802dc39537669c5a75fed
Rachel Lin <rachel.lynch.lin@gmail.com>
2019-02-07 21:27:05 -0800

    Intro to the basic data types of JavaScript

---
+++ intro_to_javascript/primitives.js
@@ -0,0 +1,46 @@
+// 5 Primitive Datatypes
+
+// Numbers
+4
+9.6
+-9
+
+// Doing math
+
+4 + 10 // 14
+1/5 // 0.2
+
+// Modulo - remainder operater
+
+10 % 3 // 1
+24 % 2 // 0
+// Strings
+
+// Single or Double quotes are OK
+"hello world"
+'hello world'
+"34"
+'53'
+
+// Concatenation
+"pea" + "nut" // "peanut"
+
+// Escape characters start with "\"
+"Singin \"Do wah diddy, diddy, dum diddy do \" "
+"This is a backslash: \\"
+
+// Strings have a length property
+"hello world".length // 11
+
+// Access individual characters using [] and an index
+"hello" [0] // "h"
+
+// Booleans
+true
+false
+
+// null and undefined
+null
+undefined
+
+
