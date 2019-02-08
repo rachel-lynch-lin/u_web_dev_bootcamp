d451de0429213b4f5732d2a369380db925be3814
Rachel Lin <rachel.lynch.lin@gmail.com>
2019-02-08 13:15:03 -0800

    Some useful built in methods for starting out with JS

---
+++ intro_to_javascript/builtInMethods.js
@@ -0,0 +1,8 @@
+// alert - creates a pop up message. Can be strings and numbers.
+
+// prompt - can promt the user to get information and then we can save the info
+
+// console.log - will print something to the cosole (good to print for your own info)
+
+
+// clear() - clears the console 
