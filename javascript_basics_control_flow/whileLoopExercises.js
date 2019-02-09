46893f70e09e30cc31d25d73c23abaa3ccc40c64
Rachel Lin <rachel.lynch.lin@gmail.com>
2019-02-09 11:29:19 -0800

    Some while loop exercises

---
+++ javascript_basics_control_flow/whileLoopExercises.js
@@ -0,0 +1,29 @@
+// While loop exercises
+    // Exercise 1
+    var num = 1;
+    
+    // Prints odd nums from 1 - 9
+    while(num <= 10) {
+        console.log(num);
+        num += 2;
+    }
+
+    // Exercise 2
+    var num2 = 1;
+
+    // Prints 4, 8, 12, 16, 20
+    while(num2 <= 20) {
+        if(num2 % 4 === 0) {
+            console.log(num2);
+        }
+        num++;
+    }
+
+    // Exercise 3
+    var num3 = 100;
+
+    // Infinite loop
+    while(num < 150) {
+        console.log(num3 + 1);
+        num--;
+    }
