4b9202414d24e9149be2efcbd4713b4185a54d5a
Rachel Lin <rachel.lynch.lin@gmail.com>
2019-02-09 11:00:01 -0800

    Intro to while loops in JS

---
+++ javascript_basics_control_flow/introToLoops.js
@@ -0,0 +1,30 @@
+// While loops
+// Printing numbers 1 - 5
+var count = 1;
+
+// Increase by 1
+while(count < 6) {
+    console.log("Count is: " + count);
+    count++;
+}
+
+// Increase by 2
+while (count < 8) {
+    console.log("Count is: " + count);
+    count+=2;
+}
+
+// Print each character in a string
+// string we are looping over
+var str = "hello";
+// first character is at index 0
+var count = 0;
+
+while(count < str.length) {
+    console.log(str[count]);
+    count++;
+}
+
+// Be careful of creating an infinite loop
+// Infinite loops ocur when the terminating condition in a loop is never true
+// Can happen in the count example if the count is never incremented
