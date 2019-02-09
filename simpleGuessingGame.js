5778f1be468207f4a186300efbf353047a489d57
Rachel Lin <rachel.lynch.lin@gmail.com>
2019-02-09 10:55:45 -0800

    Created a simple guessing game with conditionals (no loops)

---
+++ simpleGuessingGame.js
@@ -0,0 +1,22 @@
+// Create secretNumber
+
+var secretNumber = 4;
+
+// Ask user for guess
+var stringGuess = prompt("Guess a number");
+var guess = Number(stringGuess);
+
+// check if guess is right
+if(guess === secretNumber) {
+    alert("You got the right number!!!");
+}
+
+// Otherwise, check if higher
+else if(guess > secretNumber) {
+    alert("Too high. Guess again!");
+}
+
+// Otherwise, check if guess is lower
+else {
+    alert("Too low. Guess again!");
+}
