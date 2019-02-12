083390cb5bc8bbd7adc607f612ae35d6c7ef6cad
Rachel Lin <rachel.lynch.lin@gmail.com>
2019-02-12 15:08:19 -0800

    Changed the spacing to conform

--- javascript_basics_control_flow/forLoopProblemSet.js
+++ javascript_basics_control_flow/forLoopProblemSet.js
@@ -2,15 +2,15 @@
 // 1.) Print all the numbers between -10 and 19
 
 for(i = -10; i < 20; i++) {
-    console.log(i);
+  console.log(i);
 }
 
 // 2.) Print all the even numbers between 10 and 40
 
 for(j = 10; j <= 40; j += 2) {
-    if (j % 2 === 0) {
-        console.log(j);
-    }
+  if (j % 2 === 0) {
+    console.log(j);
+  }
 }
 
 // Version 2
@@ -23,14 +23,14 @@ for(j = 10; j <= 40; j += 2) {
 // 3.) Print all odd numbers between 300 and 333
 
 for(k = 300; k <= 333; k++) {
-    if (k % 2 !== 0) {
-        console.log(k);
-    }
+  if (k % 2 !== 0) {
+    console.log(k);
+  }
 }
 // 4.) Print all numbers divisble by 5 AND 3 between 5 and 50
 
 for(h = 5; h <= 50; h++) {
-    if (h % 5 === 0 && h % 3 === 0) {
-        console.log(h);
-    }
+  if (h % 5 === 0 && h % 3 === 0) {
+    console.log(h);
+  }
 }
