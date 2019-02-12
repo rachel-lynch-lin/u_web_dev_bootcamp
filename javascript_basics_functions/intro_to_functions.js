855d57fe59f2eaba8871417c0e3aadbf2c2eddeb
Rachel Lin <rachel.lynch.lin@gmail.com>
2019-02-12 15:54:54 -0800

    Intro to functions with JavaScript

---
+++ javascript_basics_functions/intro_to_functions.js
@@ -0,0 +1,15 @@
+// Functions 
+// Makes sections of code reusable.
+
+// Declare a function
+function doSomething() {
+  console.log("Hello World");
+}
+// Then call it
+doSomething();
+doSomething();
+doSomething();
+
+// Functions help to keep code DRY
+// Function names are written in camelCase as are variable names
+
