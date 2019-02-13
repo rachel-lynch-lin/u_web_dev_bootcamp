09725c1c925c59d4ab826c8f267827aebc23644b
Rachel Lin <rachel.lynch.lin@gmail.com>
2019-02-12 16:06:11 -0800

    Added section about arguments

--- javascript_basics_functions/intro_to_functions.js
+++ javascript_basics_functions/intro_to_functions.js
@@ -13,3 +13,28 @@ doSomething();
 // Functions help to keep code DRY
 // Function names are written in camelCase as are variable names
 
+// Arguments
+function square(num) {
+  console.log(num * num);
+}
+
+// Now when we call the function we need to pass in a value
+square(10); // prints 100
+square(3);  // prints 9
+square(4);  // print 16
+
+// Functions can have as many arguments as needed
+
+function area(length, width) {
+  console.log(length * width);
+}
+area(9, 2); //18
+
+function greet(person1, person2, person3) {
+  console.log("hi " + person1);
+  console.log("hi " + person2);
+  console.log("hi " + person3);
+}
+greet("Harry", "Ron", "Hermione");
+
+// If a argument is left off it would just print undefined rather than breaking the code
