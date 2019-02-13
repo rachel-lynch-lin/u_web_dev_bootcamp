09320c4d5164b4e8f7623c44ff3ee70f492a46d3
Rachel Lin <rachel.lynch.lin@gmail.com>
2019-02-12 18:12:41 -0800

    Added small quiz on functions

---
+++ javascript_basics_functions/functionsQuiz.js
@@ -0,0 +1,16 @@
+// Functions Quiz
+// What does the following code return?
+function test(x, y) {
+  return y - x
+}
+
+test(10, 40);   // 30
+
+// What does the following code return?
+function test(x) {
+  return x * 2;
+  console.log(x);
+  return x / 2;
+}
+
+test(40)    // 80
