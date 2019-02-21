f02d1bb50e9457f984ce82ce26f26071cf8ad373
Rachel Lin <rachel.lynch.lin@gmail.com>
2019-02-21 15:02:52 -0800

    Some exercises with arrays

---
+++ javascript_basics_arrays/arrays_quiz.js
@@ -0,0 +1,21 @@
+// Arrays Quiz
+
+// Exercise #1
+var numbers = [22, 67, 33, 96, 88];
+
+// What does the following line prints?
+console.log(numbers[numbers.length])
+
+// Answer: undefined
+
+// Exercise #2 
+var friendsGroups = [
+  ["Harry", "Ron", "Hermione"],
+  ["Malfoy", "Crabbe", "Goyle"],
+  ["Mooney", "Wormtail", "Prongs"]
+];
+
+// What is the result of this line:
+console.log(friendsGroups[2][0]);
+
+// Answer: Mooney
