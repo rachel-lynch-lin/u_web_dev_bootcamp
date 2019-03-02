700467685b8ab530528b06c8ec62f4cc3b2eb560
Rachel Lin <rachel.lynch.lin@gmail.com>
2019-03-02 12:00:22 -0800

    Added iteration and nested functions to todo list project

--- javascript_basics_arrays/todo_list.js
+++ javascript_basics_arrays/todo_list.js
@@ -7,16 +7,40 @@ window.setTimeout(function() {
   while(input !== "quit") {
     // handle input
     if (input == "list") {
-      console.log(todos);
+      listTodos();
     } else if (input == "new") {
-      // ask for nex todo
-      var newTodo = prompt("Enter new todo");
-      // add to todos array
-      todos.push(newTodo);
+      newTodo();
+    } else if(input === "delete") {
+      deleteTodo();
     }
     // ask again for new input
     input = prompt("What would you like to do?");
   }
   console.log("Okay, you quit the app");
-  
+
+  function listTodos() {
+    console.log("**********");
+    todos.forEach(function (todo, i) {
+      console.log(i + ": " + todo);
+    });
+    console.log("**********");
+  }
+
+  function newTodo() {
+    // ask for new todo
+    var newTodo = prompt("Enter new todo");
+    // add to todos array
+    todos.push(newTodo);
+    console.log("Added Todo");
+  }
+
+  function deleteTodo() {
+    // ask for index of todo to be deleted
+    var index = prompt("Enter the index of todo to delete");
+    // delete that index
+    // splice()
+    todos.splice(index, 1);
+    console.log("Deleted Todo");
+  }
+
 }, 500);
